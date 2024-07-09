5dcc7121e98580869f006f175049947a2e1052c5
DESKTOP-9KA65BL\gogogo <511268932@qq.com>
2024-07-09 18:01:16 +0800

    env

--- godotenv.go
+++ godotenv.go
@@ -3,6 +3,8 @@ package godotenv
 import (
 	"bytes"
 	"io"
+	"os"
+	"strings"
 )
 
 const doubleQuoteSpecialChars = "\\\n\r\"!$`"
@@ -21,4 +23,28 @@ func UnmarshalBytes(src []byte) (map[string]string, error) {
 	err := parseBytes(src, out)
 
 	return out, err
+}
+
+func readFile(filename string) (envMap map[string]string, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	return Parse(file)
+}
+
+func doubleQuoteEscape(line string) string {
+	for _, c := range doubleQuoteSpecialChars {
+		toReplace := "\\" + string(c)
+		if c == '\n' {
+			toReplace = `\n`
+		}
+		if c == '\r' {
+			toReplace = `\r`
+		}
+		line = strings.Replace(line, string(c), toReplace, -1)
+	}
+	return line
 }
